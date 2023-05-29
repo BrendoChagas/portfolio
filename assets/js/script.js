6cbc18a071a2735c7c3161551ce18a8b49947428
Brendo Chagas <brendocesar36+git@gmail.com>
2023-05-29 16:32:51 -0300

    feat: Responsivo Tablete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-function typeWrite(elemento) {
-	const textoArray = elemento.innerHTML.split("");
-	elemento.innerHTML = " ";
-	textoArray.forEach(function (letra, i) {
-		setTimeout(function () {
-			elemento.innerHTML += letra;
-		}, 75 * i);
-	});
+function escrevendoLetra(){
+	function typeWrite(elemento) {
+		const textoArray = elemento.innerHTML.split("");
+		elemento.innerHTML = " ";
+		textoArray.forEach(function (letra, i) {
+			setTimeout(function () {
+				elemento.innerHTML += letra;
+			}, 75 * i);
+		});
+	}
+	const titulo = document.querySelector(".digitando");
+	typeWrite(titulo);
 }
-const titulo = document.querySelector(".digitando");
-typeWrite(titulo);
+
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', ()=>{
+	ativaMenu.classList.toggle('fa-x');
+})
